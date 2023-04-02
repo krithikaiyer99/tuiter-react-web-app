cc7cc6f6837e90f811fd9919de5f8c91f8e14c09
krithikaiyer99 <iyer.kr@northeastern.edu>
2023-04-02 11:37:08 -0400

    chaneg back to env variable

--- src/services/tuits-services.js
+++ src/services/tuits-services.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE = "https://tuiter-node-server-app-416n.onrender.com/api";
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const findTuits = async () => {
